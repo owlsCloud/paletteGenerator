d1a48e3acb248aa16f0b152bdb5e38820cf473eb
Gerardo Garza <owlscloud@gmail.com>
2023-06-06 02:56:12 -0500

    init

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+function Navbar() {
+  return (
+    <div className="h-auto w-full flex justify-around p-4 bg-[#F7F7F7]">
+      <nav>
+        <ul className="flex gap-3">
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/">
+            <li>Saved</li>
+          </Link>
+        </ul>
+      </nav>
+
+      <div>
+        <Image src="" alt="" height={30} width={30} />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
